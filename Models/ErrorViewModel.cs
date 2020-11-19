1153caf7dc813b60280896188e92b6a10fd5545f
Nicolle Naranjo <nicolle01@javerianacali.edu.co>
2020-11-18 19:39:29 -0500

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TiendaDeCarlos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
